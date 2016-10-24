06367826e85de6b5b36f0fd1b1ce5bde7dd67415
MuluuJelekk <muluujelekk@gmail.com>
2016-10-24 19:42:19 +0800

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -26,7 +26,7 @@ sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 iptables -F
 iptables -t nat -F
-iptables -t nat -A POSTROUTING -s 10.8.0.0/16 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.200.0/16 -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 172.16.0.0/16 -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 172.1.0.0/16 -o eth0 -j MASQUERADE
 iptables-save
